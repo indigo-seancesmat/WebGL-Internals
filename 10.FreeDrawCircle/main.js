45de235294c4b1c21787894b2f8e0cf30be4b375
Sean Cesmat <sean.cesmat@indigoslate.com>
2020-04-08 16:53:39 -0700

    END OF SECTION 3 -- end of video 22 - Dynamic rendering of circles pt2

--- 10.FreeDrawCircle/main.js
+++ 10.FreeDrawCircle/main.js
@@ -8,17 +8,20 @@ var gl = utils.getGLContext(canvas);
 var vertexShader = `#version 300 es
 precision mediump float;
 in vec2 position;
+in vec3 colorFromCPU;
+out vec3 colorToFragmentShader;
 uniform float flipY;
 void main () {
     gl_Position = vec4(position.x, position.y * flipY, 0.0, 1.0);
     gl_PointSize = 10.0;
+    colorToFragmentShader = colorFromCPU;
 }`;
 var fragmentShader = `#version 300 es
 precision mediump float;
 out vec4 color;
-uniform vec3 inputColor;
+in vec3 colorToFragmentShader;
 void main () {
-    color = vec4(inputColor, 1.0);
+    color = vec4(colorToFragmentShader, 1.0);
 }`;
 
 // Step2: Create Program
@@ -26,7 +29,8 @@ var program = utils.getProgram(gl, vertexShader, fragmentShader);
 
 var getCircleCoordinates = (centerX, centerY, radiusX) => {
   var numOfPoints = 300;
-  var circleCoords = [];
+  var circleCoords = [],
+    colors = [];
   var radiusY = (radiusX / gl.canvas.height) * gl.canvas.width;
   for (var i = 0; i < numOfPoints; i++) {
     // 2*Math.PI*r
@@ -34,8 +38,12 @@ var getCircleCoordinates = (centerX, centerY, radiusX) => {
     var x = centerX + radiusX * Math.cos(circumference);
     var y = centerY + radiusY * Math.sin(circumference);
     circleCoords.push(x, y);
+    colors.push(Math.random(), Math.random(), Math.random());
   }
-  return circleCoords;
+  return {
+    circleCoords,
+    colors,
+  };
 };
 
 initializeEvents(gl, (startX, startY, endX, endY) => {
@@ -52,7 +60,8 @@ initializeEvents(gl, (startX, startY, endX, endY) => {
   var centerX = v.startX + (v0to2.endX - v0to2.startX) / 2;
   var centerY = v.startY + (v0to2.endY - v0to2.startY) / 2;
   var radiusX = (v0to2.endX - v0to2.startX) / 2;
-  var vertices = getCircleCoordinates(centerX, centerY, radiusX);
+  var verticesData = getCircleCoordinates(centerX, centerY, radiusX);
+  var vertices = verticesData.circleCoords;
   console.log(vertices);
   var data = new Float32Array(vertices);
   var buffer = utils.createAndBindBuffer(
@@ -60,6 +69,15 @@ initializeEvents(gl, (startX, startY, endX, endY) => {
     gl.ARRAY_BUFFER,
     gl.STATIC_DRAW,
     data
+  );
+
+  var colors = verticesData.colors;
+  var colorData = new Float32Array(colors);
+  var colorBuffer = utils.createAndBindBuffer(
+    gl,
+    gl.ARRAY_BUFFER,
+    gl.STATIC_DRAW,
+    colorData
   );
 
   // Step4: Linking CPU & GPU
@@ -70,11 +88,14 @@ initializeEvents(gl, (startX, startY, endX, endY) => {
     buffer: buffer,
     dims: 2,
   });
+  utils.linkGPUAndCPU(gl, {
+    program: program,
+    gpuVariable: "colorFromCPU",
+    buffer: colorBuffer,
+    dims: 3,
+  });
   var flipY = gl.getUniformLocation(program, "flipY");
   gl.uniform1f(flipY, -1.0);
-
-  var inputColor = gl.getUniformLocation(program, "inputColor");
-  gl.uniform3fv(inputColor, [0.1, 0.2, 0.3]);
 
   // Step5: Render the rectangle
   gl.drawArrays(gl.POINTS, 0, vertices.length / 2);
